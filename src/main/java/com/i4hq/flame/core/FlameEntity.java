73e99d0c9f20ac45ec78dd7f9b8f24cb8dcd20a7
rodmoten <rod-i4hq@users.noreply.github.com>
2017-01-05 22:50:38 -0500

    : Create code to convert a TED FlameEntity into a SitaWare Symbol
    Changed the attribute path not to include the entity ID prefix

--- src/main/java/com/i4hq/flame/core/FlameEntity.java
+++ src/main/java/com/i4hq/flame/core/FlameEntity.java
@@ -1,52 +1,39 @@
 package com.i4hq.flame.core;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.Stack;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.opencsv.CSVReader;
 
 public class FlameEntity {	
 
 	private static Logger logger = LoggerFactory.getLogger(FlameEntity.class);
 
 	private final Map<String, AttributeValue> attributes = new HashMap<>();
-	private final static GsonBuilder gsonBuilder = new GsonBuilder();
-	private static final char ATTIRBUTE_PATH_SEPARATOR = ':';
+	public static final char ATTIRBUTE_PATH_SEPARATOR = ':';
 	public static final String ENITY_ID_ATTIRBUTE_PATH_SEPARATOR = "" + ATTIRBUTE_PATH_SEPARATOR + ATTIRBUTE_PATH_SEPARATOR;
 	private final String id;
 	private final String entityIdPrefix;
-	private static final String ATTRIBUTE_TYPE_EXPR_SEPARATOR = ":::";
+	public static final String ATTRIBUTE_TYPE_EXPR_SEPARATOR = ":::";
 	private GeospatialPosition geospatialPosition;
 	private String hash = "";
 
 
-	private FlameEntity(String id) {
+	FlameEntity(String id) {
 		this.id = id;
 		entityIdPrefix = id + ENITY_ID_ATTIRBUTE_PATH_SEPARATOR;
 	}
-
-
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#hashCode()
@@ -89,17 +76,7 @@ public class FlameEntity {
 
 
 
-	/**
-	 * Create an new Flame Entity object.  
-	 * @param entityId
-	 * @param entityType
-	 * @param attributes - the attributes of the entity. The m
-	 * @return A new Flame Entity without any attributes.
-	 */
-	public static FlameEntity createEntity(String entityId) {
-		FlameEntity entity = new FlameEntity(entityId);
-		return entity;
-	}
+	
 
 	//	/**
 	//	 * @param entityType
@@ -122,7 +99,7 @@ public class FlameEntity {
 	 * @param longitude
 	 * @param latitude
 	 */
-	public void setLocation(long longitude, long latitude) {
+	public void setLocation(double longitude, double latitude) {
 		this.geospatialPosition = new GeospatialPosition(longitude, latitude);
 	}
 
@@ -146,233 +123,8 @@ public class FlameEntity {
 
 	}
 
-	/**
-	 * Create an entity from each line of a CSV file. One of the columns contains the entity IDs. 
-	 * If two rows contains the same entity ID, then only the first row is read.
-	 * The method assumes the first row contains the column names.
-	 * 
-	 * 
-	 * @param entityIdColumn - name of the column that has the entity ID.
-	 * @param attributeIdFactory
-	 * @param csvFile
-	 * @param skipDuplicates
-	 * @return Returns a list of the entities created. The order of the entities in the list is the same as the order of the file.
-	 * @throws IOException 
-	 * @exception RuntimeException is thrown if no column matches the entityIdColumn.
-	 */
-	public static List<FlameEntity> createFromCsv (String entityIdColumn, AttributeIdFactory attributeIdFactory, File csvFile) throws IOException{
-		// Create a reader to read each line.
-		CSVReader reader = new CSVReader(new FileReader(csvFile));
-		List<FlameEntity> entities = new LinkedList<>();
-
-		// We use this variable to ensure we only get one entity for each entity ID. 
-		Set<String> readEntities = new HashSet<>();
-
-		try {
-			int lineNumber = 1;
-			// Get the columns.
-			String [] columns;
-			long[] attributeIds;
-			columns = reader.readNext();
-			if (columns == null || columns.length == 0){
-				logger.error("The first line doesn't contain the column headers in  {}", csvFile);
-				throw new RuntimeException("No columns in CSV");
-			}
-
-			// For some reason, the first character in the CSV is 65279. As a result, we remove it from the first column header.
-			if (columns[0].charAt(0) == 65279){
-				columns[0] = columns[0].substring(1);
-			}
-			// Throw an exception if the entity ID column is not present.
-			// In addition, get the attribute IDs for each of the column names. We will use them later when we read each row.
-			attributeIds = new long[columns.length];
-			int entityIdColumnIndex = -1;
-			for (int i = 0; i < columns.length; i++){
-				String columnName = columns[i];
-				if (entityIdColumn.equalsIgnoreCase(columnName)) {
-					entityIdColumnIndex = i;
-				}
-				attributeIds[i] = attributeIdFactory.getAttributeId(columnName);
-			}
-			if (entityIdColumnIndex < 0) {
-				throw new RuntimeException(String.format("Entity ID column '%s' not present in '%s'", entityIdColumn, csvFile.getAbsolutePath()));
-			}
-
-			// Create an entity for each line.
-			String[] nextLine;
-			while ((nextLine = reader.readNext()) != null) {
-				lineNumber++;
-				int numOfColumns = Math.min(columns.length, nextLine.length);
-				// Skip blank lines.
-				if (numOfColumns == 0) {
-					continue;
-				}
-				if (numOfColumns <= entityIdColumnIndex) {
-					logger.warn("Skipping entity at line {} in file '{}' because it doesn't have an entity ID.", lineNumber, csvFile.getName());
-				}
-				String entityId = nextLine[entityIdColumnIndex];
-				FlameEntity entity = createEntity(entityId);
-				// skip the entity if one with the same ID was previously read.
-				if (readEntities.contains(entity.getId())) {
-					continue;
-				}
-				
-				for (int i = 0; i < numOfColumns; i++){
-					// Don't make the entity ID an attribute.
-					if (i == entityIdColumnIndex) {
-						continue;
-					}
-					long attributeId = attributeIds[i];
-
-					String attributeName = createAttributePathName(entity, null, attributeId);
-					Object value = nextLine[i];
-					entity.addAttribute(attributeName, value, AttributeType.STRING);
-				}
-				readEntities.add(entity.getId());
-				entities.add(entity);
-
-			}
-		} catch (IOException ex) {
-			logger.error("Error reading CSV file: {}", csvFile);
-			throw ex;
-		}
-
-		finally {
-			reader.close();
-		}
-
-
-		return entities;
-
-	}
-	/**
-	 * @param entityIdFactory
-	 * @param attributeIdFactory
-	 * @param jsonText
-	 * @return Returns a Flame entity created from JSON contained in a string.
-	 */
-	public static FlameEntity createFromJson (EntityIdFactory entityIdFactory, AttributeIdFactory attributeIdFactory, String jsonText){
-		Stack<Long> parentPath = new Stack<>();
-		FlameEntity entity = new FlameEntity(entityIdFactory.createId(jsonText));
-		Gson gson =gsonBuilder.create();
-
-		JsonObject jsonObj = gson.fromJson(jsonText, JsonObject.class);
-		createFromJson(entity, entityIdFactory, attributeIdFactory, parentPath, jsonObj);
-
-		return entity;
-	}
-
-	/**
-	 * @param entity
-	 * @param entityIdFactory
-	 * @param attributeIdFactory
-	 * @param parentPath
-	 * @param jsonObj
-	 */
-	private static void createFromJson(FlameEntity entity, EntityIdFactory entityIdFactory, AttributeIdFactory attributeIdFactory,
-			Stack<Long> parentPath, JsonObject jsonObj) {
-
-
-		for (Entry<String, JsonElement> jsonElement : jsonObj.entrySet()) {
-			// Get the attribute name 
-			String attributeName = jsonElement.getKey();
-			if (!validAttributeName(attributeName)) {
-				throw new IllegalArgumentException (String.format("Attribute name is not valid: '{}'", attributeName));
-			}
-
-			// Get the attribute value. 
-			JsonElement attributeValue = jsonElement.getValue();
-			//TODO handle arrays
-			if (attributeValue.isJsonArray()) {
-				throw new IllegalArgumentException ("JSON must not contain any arrays");
-			}
-			boolean isScalar = attributeValue.isJsonPrimitive() || attributeValue.isJsonNull();
-			long attributeId = attributeIdFactory.getAttributeId(attributeName);
-
-			// If this is a primitive JSON element, then add it to the entity.
-			if (isScalar) {
-				String attributePathName = createAttributePathName(entity, parentPath, attributeId);
-				if (attributeValue.isJsonNull()) {
-					entity.attributes.put(attributePathName, new AttributeValue(null, getAttributeTypeOfScalarValue(null)));
-				} else {
-					entity.attributes.put(attributePathName, new AttributeValue(attributeValue.getAsString(), getAttributeTypeOfScalarValue(attributeValue.getAsJsonPrimitive())));
-				}
-			} else {
-				// Add the JSON element's attribute ID to the stack and continue with its children.
-				parentPath.push(attributeId);
-				createFromJson (entity, entityIdFactory, attributeIdFactory, parentPath, attributeValue.getAsJsonObject());
-			}
-		}
-		if (!parentPath.empty()) {
-			parentPath.pop();
-		}
-
-	}
-
-
 	public String getEntityIdPrefix() {
 		return entityIdPrefix ;
-	}
-
-	/**
-	 * @param attributeValue
-	 * @return
-	 */
-	private static AttributeType getAttributeTypeOfScalarValue(JsonPrimitive attributeValue) {
-		if (attributeValue == null){
-			return AttributeType.STRING;
-		}
-		if (attributeValue.isBoolean()) {
-			return AttributeType.BOOLEAN;
-		}
-		if(attributeValue.isNumber()) {
-			return AttributeType.NUMBER;
-		}
-		// Should be a JSON string or a JSON null.
-		return AttributeType.STRING;
-
-	}
-	/**
-	 * @param attributeName
-	 * @return Returns true if and only if the attribute name is valid.
-	 */
-	private static boolean validAttributeName(String attributeName) {
-		if (attributeName == null) {
-			return false;
-		}
-		int len = attributeName.length();
-		for (int i = 0; i < len; i++){
-			if (attributeName.charAt(i) == ATTIRBUTE_PATH_SEPARATOR) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * @param entity 
-	 * @param parentPath
-	 * @param attributeId
-	 * @return Returns the attribute path name 
-	 */
-	private static String createAttributePathName(FlameEntity entity, Stack<Long> parentPath, long attributeId) {
-		StringBuilder attributePathName = new StringBuilder(entity.getEntityIdPrefix());
-		int count = 0;
-		if (parentPath != null){
-			for (long ancestorAttributeId : parentPath) {
-				if (count > 0) {
-					attributePathName.append(ATTIRBUTE_PATH_SEPARATOR);
-				}
-				count++;
-				attributePathName.append(ancestorAttributeId);
-			}
-		}
-		if (count > 0) {
-			attributePathName.append(ATTIRBUTE_PATH_SEPARATOR);
-		}
-		attributePathName.append(attributeId);
-
-		return attributePathName.toString();
 	}
 
 	/**
@@ -384,7 +136,7 @@ public class FlameEntity {
 
 		
 	/**
-	 * @param attributePath
+	 * @param attributePath - 
 	 * @return Returns the value for this attribute. null will be return if the value of the attribute is null or if the entity doesn't contain the attribute.
 	 */
 	public AttributeValue getAttribute(String attributePath) {
@@ -414,7 +166,7 @@ public class FlameEntity {
 		List<String> attributeTypExpression = new LinkedList<>();
 
 		for (Entry<String, AttributeValue> entry : this.attributes.entrySet()) {
-			attributeTypExpression.add(entry.getKey().substring(getEntityIdPrefix().length()) + ATTRIBUTE_TYPE_EXPR_SEPARATOR + entry.getValue().getType());
+			attributeTypExpression.add(entry.getKey() + ATTRIBUTE_TYPE_EXPR_SEPARATOR + entry.getValue().getType());
 		}
 		Collections.sort(attributeTypExpression);
 		for (String attributeTypeExpr : attributeTypExpression){
--- src/test/java/com/i4hq/flame/FlameEntityTest.java
+++ src/test/java/com/i4hq/flame/FlameEntityTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import com.i4hq.flame.core.AttributeIdFactory;
 import com.i4hq.flame.core.EntityIdFactory;
 import com.i4hq.flame.core.FlameEntity;
+import com.i4hq.flame.core.FlameEntityFactory;
 import com.i4hq.flame.in_memory.InMemoryAttributeIdFactory;
 import com.i4hq.flame.in_memory.InMemoryEntityIdFactory;
 
@@ -42,13 +43,12 @@ properties.geometry.angle 1::4:8:9
 		 */
 		int expectedNumOfAttributes = 9;
 		StringBuilder jsonText = readJsonFromFile("src/test/resources/entity-positive-test.json");
-		FlameEntity entity = FlameEntity.createFromJson(entityIdFactory, attributeIdFactory, jsonText.toString());
-		String expectedEntityId = "262";
+		FlameEntity entity = FlameEntityFactory.createFromJson(entityIdFactory, attributeIdFactory, jsonText.toString());
 		assertEquals("num of attributes", expectedNumOfAttributes, entity.size());
 		String expectedType = "1:::NUMBER\n2:::STRING\n3:::STRING\n4:5:::BOOLEAN\n4:6:::STRING\n4:7:1:::STRING\n4:7:3:::STRING\n4:8:1:::STRING\n4:8:9:::NUMBER\n";
 		assertEquals("type",expectedType, entity.getType());
-		assertEquals("id(2)","9", entity.getAttribute(expectedEntityId + "::2").getValue());
-		assertEquals("properties.attrs.name(4:7:3)","Sector", entity.getAttribute(expectedEntityId + "::4:7:3").getValue());
+		assertEquals("id(2)","9", entity.getAttribute("2").getValue());
+		assertEquals("properties.attrs.name(4:7:3)","Sector", entity.getAttribute("4:7:3").getValue());
 		
 		// attribute not in entity
 		assertEquals("properties.attrs.type(4:10:11)",null, entity.getAttribute("4:10:11"));		
@@ -58,34 +58,34 @@ properties.geometry.angle 1::4:8:9
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreateFromJson_arrays() throws Exception {
 		StringBuilder jsonText = readJsonFromFile("src/test/resources/entity-negative-test1.json");
-		FlameEntity.createFromJson(entityIdFactory, attributeIdFactory, jsonText.toString());
+		FlameEntityFactory.createFromJson(entityIdFactory, attributeIdFactory, jsonText.toString());
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreateFromJson_invalidFieldName() throws IOException {
 		StringBuilder jsonText = readJsonFromFile("src/test/resources/entity-negative-test2.json");
-		FlameEntity.createFromJson(entityIdFactory, attributeIdFactory, jsonText.toString());
+		FlameEntityFactory.createFromJson(entityIdFactory, attributeIdFactory, jsonText.toString());
 	}
 
 	@Test
 	public void testCreateFromCsv() throws Exception {
 		File csvFile = new File("src/test/resources/few-dummy-entities-with-dups.csv");
 		
-		List<FlameEntity> actualEntities = FlameEntity.createFromCsv("entity_id", attributeIdFactory, csvFile);
+		List<FlameEntity> actualEntities = FlameEntityFactory.createFromCsv("entity_id", attributeIdFactory, csvFile, "LONGITUDE", "LATITUDE", false);
 		int expectedNumOfEntities = 5;
 		assertEquals("number of entities", expectedNumOfEntities, actualEntities.size());
 		// Assert the first entity
-		String[][] firstEntity = {{"type", "0AFFD7D2-6366-47F8-945C-500FAC21C67E::2", "Equipment"}, {"name", "0AFFD7D2-6366-47F8-945C-500FAC21C67E::3","Trailer, Generator"}};
+		String[][] firstEntity = {{"type", "2", "Equipment"}, {"name", "3","Trailer, Generator"}};
 		assertAttribute(0,0,firstEntity, actualEntities);
 		assertAttribute(1,0,firstEntity, actualEntities);
 
 		// Assert the 3rd entity
-		String[][] thirdEntity = {{"ANALYST_COMMENTS", "00914273-E995-42F0-A428-77170495311A::8", "NULL"}, {"name", "00914273-E995-42F0-A428-77170495311A::3","LJUBAN KONSTANTIN STANKIC"}};
+		String[][] thirdEntity = {{"ANALYST_COMMENTS", "8", "NULL"}, {"name", "3","LJUBAN KONSTANTIN STANKIC"}};
 		assertAttribute(0,2,thirdEntity, actualEntities);
 		assertAttribute(1,2,thirdEntity, actualEntities);
 		
 		// Assert the last entity
-		String[][] lastEntity = {{"LATITUDE", "11F7723C-6F4A-46B2-93A0-3F3E18D95910::4", "40.12242653"}, {"name", "11F7723C-6F4A-46B2-93A0-3F3E18D95910::3","352 AD BN"}};
+		String[][] lastEntity = {{"LATITUDE", "4", "40.12242653"}, {"name", "3","352 AD BN"}};
 		assertAttribute(0,4,lastEntity, actualEntities);
 		assertAttribute(1,4,lastEntity, actualEntities);
 	}
